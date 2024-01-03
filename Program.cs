0d63c82b18ace0b04ac312d5dd19fbe702249401
cuu-vnpt <kunnobita97@gmail.com>
2024-01-03 08:45:51 +0700

    new file

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int choice = -1;
+        while (choice !=0)
+        {
+            Console.WriteLine("Nhap lua chon");
+        choice = int.Parse(Console.ReadLine());
+            switch(choice)
+            {
+                case 1: 
+                {
+                    //chuyen tu C sang F
+                    Console.WriteLine(CelsiusToFahrenheit());
+                }
+                break;
+
+                case 2: 
+                {
+                    //Chuyen tu F sang C
+                    Console.WriteLine(FahrenheitToCelsius());
+                }
+                break;
+                case 0: 
+                {
+                    //Thoat ra 
+                }
+                break;
+
+                default:
+                {
+                    Console.WriteLine("Nhap lai");
+                }
+                break;
+            }
+        }
+    }
+    static double CelsiusToFahrenheit()
+    {
+                    Console.WriteLine("Nhap do C");
+                    int celsius = int.Parse(Console.ReadLine());
+                    double fahrenheit = 9/5* celsius+32; 
+                    return fahrenheit;
+    }
+    static double FahrenheitToCelsius()
+    {
+                    Console.WriteLine("Nhap do C");
+                    int fahrenheit = int.Parse(Console.ReadLine());
+                    double celsius = 5/9*(fahrenheit-32);
+                    return celsius;
+    }
+}
